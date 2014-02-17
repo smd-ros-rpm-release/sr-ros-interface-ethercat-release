39e5c2ffe342dfbfb82807d8e3df3853218c62a8
Toni Oliver <toni.oliver.d@gmail.com>
2014-02-17 19:43:14 +0000

    Trimmed the branch to only the sr_robot_lib sub directory

--- sr_edc_muscle_tools/bin/stop_all_controllers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for name in $(rosrun pr2_controller_manager pr2_controller_manager list | grep running | cut -d ' ' -f 1); do
-    rosrun pr2_controller_manager pr2_controller_manager stop $name
-done

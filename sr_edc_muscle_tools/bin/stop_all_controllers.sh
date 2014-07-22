d39f0643c6c68a65060e98352d206b5e48ea3e55
shadowmanos <manos@shadowrobot.com>
2014-07-22 17:02:59 +0100

    Trimmed the branch to only the shadow_robot_ethercat sub directory

--- sr_edc_muscle_tools/bin/stop_all_controllers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for name in $(rosrun controller_manager controller_manager list | grep running | cut -d ' ' -f 1); do
-    rosrun controller_manager controller_manager stop $name
-done
